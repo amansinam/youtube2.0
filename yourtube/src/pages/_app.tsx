f2c175c61769bb806890f5b28c43dba5b6ffeada
Aman Sinam <2023csb1098@iitrpr.ac.in>
2026-09-06 11:26:28 +0530

    Fix document title hydration mismatch

--- yourtube/src/pages/_app.tsx
+++ yourtube/src/pages/_app.tsx
@@ -3,12 +3,15 @@ import Sidebar from "@/components/Sidebar";
 import { Toaster } from "@/components/ui/sonner";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { UserProvider } from "../lib/AuthContext";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <div className="min-h-screen bg-white text-black">
+      <Head>
         <title>Your-Tube Clone</title>
+      </Head>
+      <div className="min-h-screen bg-white text-black">
         <Header />
         <Toaster />
         <div className="flex">
